2d708ad0a5a5af2ed76377dbd3bb8c9e923d067b
Broveku <gearhearjordan@gmail.com>
2021-09-08 12:58:45 -0700

    adds to middleware

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,4 +1,5 @@
 const Scheme = require('../schemes/scheme-model')
+const db = require('../../data/db-config')
 
 
 
@@ -12,15 +13,17 @@ const Scheme = require('../schemes/scheme-model')
 */
 const checkSchemeId = async (req, res, next) => {
   try {
-    const scheme = await Scheme.findById(req.params.id)
-      if (!scheme) {
-        res.status(404).json({message: 'id is not found'})
-      }else {
-        req.scheme = scheme
+    const existing = await db('schemes')
+      .where('scheme_id', req.params.scheme_id)
+      .first()
+
+      if (!existing){
+        next({status: 404, message: `scheme with scheme_id ${req.params.scheme_id} not found`})
+      }else{
         next()
       }
       }catch (err){
-        next()
+        next(err)
     }
   
   
