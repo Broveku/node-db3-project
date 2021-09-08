fdb41f80b3eea96ec8cfedd5db7c59d12c1dc071
Broveku <gearhearjordan@gmail.com>
2021-09-08 13:45:49 -0700

    wip

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -117,7 +117,7 @@ async function findById(scheme_id) { // EXERCISE B
   */
 }
 
-function findSteps(scheme_id) { // EXERCISE C
+async function findSteps(scheme_id) { // EXERCISE C
   /*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
@@ -137,7 +137,14 @@ function findSteps(scheme_id) { // EXERCISE C
           "scheme_name": "Get Rich Quick"
         }
       ]
-  */
+  */ 
+  const rows = await db('schemes as sc')
+        .LeftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+        .select('st.step_id', 'st.step_number', 'instruction', 'sc.scheme_id')
+        .where('sc.scheme_id', scheme_id)
+        .orderBy('st.step_number')
+    if (!rows[0].step_id) return []
+    return rows
 }
 
 function add(scheme) { // EXERCISE D
