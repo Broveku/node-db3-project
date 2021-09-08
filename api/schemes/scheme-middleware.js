1b22c4225a1c7274f7ced752284f2db1d865b904
Broveku <gearhearjordan@gmail.com>
2021-09-08 14:07:18 -0700

    wip

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,16 +1,5 @@
-const Scheme = require('../schemes/scheme-model')
 const db = require('../../data/db-config')
 
-
-
-/*
-  If `scheme_id` does not exist in the database:
-
-  status 404
-  {
-    "message": "scheme with scheme_id <actual id> not found"
-  }
-*/
 const checkSchemeId = async (req, res, next) => {
   try {
     const existing = await db('schemes')
@@ -25,19 +14,8 @@ const checkSchemeId = async (req, res, next) => {
       }catch (err){
         next(err)
     }
-  
-  
-  }
+}
 
-
-/*
-  If `scheme_name` is missing, empty string or not a string:
-
-  status 400
-  {
-    "message": "invalid scheme_name"
-  }
-*/
 const validateScheme = (req, res, next) => {
   const { scheme_name } = req.body
   if (
@@ -51,19 +29,8 @@ const validateScheme = (req, res, next) => {
     }
 }
 
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
-
-  status 400
-  {
-    "message": "invalid step"
-  }
-*/
-const validateStep = (req, res, next) => {
-  
+const validateStep = (req, res, next) => { 
   const { instructions, step_number } = req.body
-
   if (
     instructions === undefined ||
     typeof instructions !== 'string' ||
